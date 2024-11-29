b83b348179cb460835c6b534c32ea5e4ca8c9a5f
monchobung <bunger@udesa.edu.ar>
2024-11-28 23:11:06 -0300

    agregue que la categoria de una receta sea clickeable y te lleve a category

--- js/receta.js
+++ js/receta.js
@@ -5,7 +5,6 @@ function getQueryParam(param) {
 
 let ProductoId = getQueryParam('id');
 
-
 if (ProductoId) {
     let url = `https://dummyjson.com/recipes/${ProductoId}`;
 
@@ -14,35 +13,33 @@ if (ProductoId) {
             return res.json();
         })
         .then(function(data) {
-           
             console.log(data);
 
-            
-         
-
             let fotoReceta = document.querySelector('.foto');
-            let titulo = document.querySelector('.titulo')
-            let informacionReceta = document.querySelector(`.instrucciones`)
-            let tiempoCoccion = document.querySelector('.tiempoCoccion')
-            let categoria = document.querySelector('.categoria')
+            let titulo = document.querySelector('.titulo');
+            let informacionReceta = document.querySelector('.instrucciones');
+            let tiempoCoccion = document.querySelector('.tiempoCoccion');
+            let categoria = document.querySelector('.categoria');
 
-          
+            fotoReceta.src = data.image;
+            titulo.textContent = data.name;
+            informacionReceta.textContent = data.instructions;
+            tiempoCoccion.textContent = 'Tiempo Cocción: ' + data.cookTimeMinutes + ' minutos';
 
-            fotoReceta.src =data.image;
-            titulo.textContent =data.name
-            informacionReceta.textContent = data.instructions
-            tiempoCoccion.textContent ='Tiempo Coccion: ' + data.cookTimeMinutes + ' minutos'
-            categoria.textContent = 'Categoria: ' + data.tags
-
-            for (let index = 0; index < data.tags.length; index++) {
-                const tag = data.tags[index];
-                // "<a href='./result-categ.?'"
-            }
             
-
-
-
-
+            let tags = data.tags; 
+            if (tags) {
+                let tagsHTML = '';
+                for (let i = 0; i < tags.length; i++) {
+                    tagsHTML += `<a href="./category.html?tag=${tags[i]}">${tags[i]}</a>`;
+                    if (i < tags.length - 1) {
+                        tagsHTML += ', '; 
+                    }
+                }
+                categoria.innerHTML = 'Categoría: ' + tagsHTML;
+            } else {
+                categoria.textContent = 'La receta no tien categoría';
+            }
         })
         .catch(function(e) {
             console.log(e);
